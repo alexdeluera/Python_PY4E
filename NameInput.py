0d1596c8caabf8e7a85568a24c6cf2d9b8a56b5b
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2023-09-21 16:16:28 -0400

    Includes instructions and solution

--- NameInput.py
+++ NameInput.py
@@ -1,4 +1,4 @@
-#2.2 Write a program that uses input to prompt a user for their name and then welcomes them. 
+#2.2 Write a program that uses input to prompt a user for their name and then welcomes them.
 
 nam=input('What is your name? ')
 print('Hello ', nam)

aa1cd1b77f87e9aff2b270672ccb337787daa4c1
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2023-09-21 16:18:43 -0400

    Includes instructions and solution.

--- HoursInput.py
+++ HoursInput.py
@@ -1,3 +1,7 @@
+# 2.3 Write a program to prompt the user for hours and rate per hour using 
+# input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to 
+# test the program (the pay should be 96.25).
+
 hours=input('How many hours have you worked? ')
 hours=int(hours)
 rate=input('What is your hourly rate? ')

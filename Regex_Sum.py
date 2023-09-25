74131de1669e040b83f5f5924eacaa801eb2da6a
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2023-09-25 15:54:26 -0400

    Update Regex_Sum.py
    Includes instructions and solution

--- Regex_Sum.py
+++ Regex_Sum.py
@@ -1,6 +1,6 @@
-#The basic outline of this problem is to read the file, look for integers 
-#using the re.findall(), looking for a regular expression of '[0-9]+' and
-#then converting the extracted strings to integers and summing up the integers.
+# The basic outline of this problem is to read the file, look for integers 
+# using the re.findall(), looking for a regular expression of '[0-9]+' and
+# then converting the extracted strings to integers and summing up the integers.
 
 #fname=input("Please input the file name:")
 import re
